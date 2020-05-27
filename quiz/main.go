8a1cd8051be21b27e3192104335e198e1a45b363
Christopher Stewart <chris@notlinear.com>
2020-05-26 18:01:11 -0700

    I think I am done

--- quiz/main.go
+++ quiz/main.go
@@ -7,63 +7,10 @@ import (
 	"fmt"
 	"math/rand"
 	"strconv"
+	"strings"
 	"time"
 	"os"
 )
-
-var messages = make(chan string)
-
-func main() {
-	csvFilename := flag.String("csv", "problems.csv", "a CSV file in the format of 'question,answer'")
-	timerSeconds := flag.Int("seconds", 30, "Set in seconds the quiz timer duration")
-	randomizeProblems := flag.Bool("randomize", false, "Randomize the order of the quiz questions")
-	flag.Parse()
-	
-	file, err := os.Open(*csvFilename)
-	if err != nil {
-		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename))
-	}
-	r := csv.NewReader(file)
-	lines, err := r.ReadAll()
-	if err != nil {
-		exit("Failed to parse the provided CSV file")
-	}
-
-	problems := parseLines(lines)
-
-	fmt.Printf("%T\n", problems)
-
-	if *randomizeProblems {
-		rand.Seed(time.Now().UnixNano())
-		rand.Shuffle(len(problems), func(i, j int) {
-			problems[i], problems[j] = problems[j], problems[i]
-		})
-	}
-
-	fmt.Println(problems)
-
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Timed Pop Quiz!")
-	fmt.Printf("%d seconds set for timer\n", *timerSeconds)
-  	fmt.Print("Press 'Enter' to start quiz...")
-  	reader.ReadBytes('\n')
-
-  	go quizTimer(*timerSeconds)
-  	go quiz(problems)
-
-  	for {
-  		select {
-  		case msg := <- messages:
-  			exit(msg)
-
-  		}
-  	}
-
-//	quizTimeout := time.Second * time.Duration(timerSeconds)
-//	quizTimer := time.NewTimer(quizTimeout)
-	//quizTimerC = quizTimer.C
-
-}
 
 type problem struct {
 	q string
@@ -81,38 +28,160 @@ func parseLines(lines [][]string) []problem {
 	return ret
 }
 
-func exit(msg string) {
-	fmt.Println(msg)
-	os.Exit(1)
+func describe(i interface{}) {
+	fmt.Printf("(%+v, %T)\n", i, i)
 }
 
+func exit(msg string, code int) {
+	fmt.Println(msg)
+	os.Exit(code)
+}
 
-//[]main.problem
+func takeQuiz(quizProblems []problem, channel chan problem) {
 
-func quiz(quizProblems []problem) {
-	correct := 0
-	wrong := 0
-
+	// just to have nice padded formating of problems
 	width := len(strconv.Itoa(len(quizProblems)))
+
+	var correct string
+	var sendAnswer problem
+	var answer string
 
 	for i, p := range quizProblems {
 		fmt.Printf("Problem #%-*d: %s = \n", width, i+1, p.q)
-		var answer string
 		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			//fmt.Println("Correct")
-			correct++
+		if strings.EqualFold(strings.TrimSpace(answer), strings.TrimSpace(p.a)) {
+			correct = "CORRECT"
 		} else {
-			//fmt.Println("WRONG")
-			wrong++
+			correct = "WRONG"
 		}
+		sendAnswer = problem{
+			q: correct,
+			a: answer,
+		}
+		channel <- sendAnswer
 	}
-	fmt.Printf("You scored %d out of %d.\n", correct, len(quizProblems))
-	os.Exit(0)
+	close(channel)
 }
 
-func quizTimer(quizDuration int) {
+func startTimer(quizDuration int, done chan bool) {
 	time.Sleep(time.Second * time.Duration(quizDuration))
-	messages <- "\n** Quiz timer elapsed! **"
-	//fmt.Println("elapsed!")
+	done <- true
+	close(done)
 }
+
+func gradeQuiz(problems []problem, answers []problem) {
+	var correctCount int
+	// just to have nice padded formating of problems
+	fmt.Println("\n******** Quiz Restults ********")
+	width := len(strconv.Itoa(len(problems)))
+	for i, p := range problems {
+		fmt.Printf("Problem #%-*d: %s =\n", width, i+1, p.q)
+		fmt.Printf("Correct Answer: %s\n", p.a)
+		fmt.Printf("  Given Answer: %s\n", answers[i].a)
+		fmt.Printf("Your Answer is: %s\n\n", answers[i].q)
+		if answers[i].q == "CORRECT" {
+			correctCount++
+		}
+	}
+
+	fmt.Printf("\nYou scored %d out of %d.\n\n", correctCount, len(problems))
+}
+
+func main() {
+	csvFilename := flag.String("csv", "problems.csv", "a CSV file in the format of 'question,answer'")
+	timerSeconds := flag.Int("seconds", 30, "Set in seconds the quiz timer duration")
+	randomizeProblems := flag.Bool("randomize", false, "Randomize the order of the quiz questions")
+	debugOutput := flag.Bool("debug", false, "Output some variables for debugging")
+	flag.Parse()
+	
+	
+	file, err := os.Open(*csvFilename)
+	codeFileOpenFail := 10
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file: %s\n", *csvFilename), codeFileOpenFail)
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	codeFileReadFail := 11
+	if err != nil {
+		exit("Failed to parse the provided CSV file", codeFileReadFail)
+	}
+	defer file.Close()
+
+	quizProblems := parseLines(lines)
+	if *randomizeProblems {
+		rand.Seed(time.Now().UnixNano())
+		rand.Shuffle(len(quizProblems), func(i, j int) {
+			quizProblems[i], quizProblems[j] = quizProblems[j], quizProblems[i]
+		})
+	}
+
+  	var quizAnswers = make([]problem, len(quizProblems))
+  	var answerNum int
+  	var codeQuizDone int
+
+  	// Pre-Populate quizAnswers with WRONG answers for faster calculation
+  	for i := range quizProblems {
+  		quizAnswers[i] = problem{
+  			q: "WRONG",
+  			a: "**NO ANSWER GIVEN**",
+  		}
+  	}
+
+	if *debugOutput {
+		describe(*csvFilename)
+		describe(*timerSeconds)
+		describe(*randomizeProblems)
+		describe(lines)
+		describe(quizProblems)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("\n*** Timed Pop Quiz! ***")
+	fmt.Printf("%d seconds set for timer\n", *timerSeconds)
+  	fmt.Print("Press 'Enter' to start quiz...\n\n")
+  	reader.ReadBytes('\n')
+	
+  	timerDone := make(chan bool)
+	answersChannel := make(chan problem)
+
+  	go startTimer(*timerSeconds, timerDone)
+  	go takeQuiz(quizProblems, answersChannel)
+
+  	isRunning := true
+  	for isRunning {
+  		select {
+  		case RecievedAnswer, ok := <- answersChannel:
+  			if ok {
+	  			quizAnswers[answerNum] = RecievedAnswer
+  				answerNum++
+  				if *debugOutput {describe(answersChannel)}
+  			} else {
+  				isRunning = false
+  			}
+  		case <- timerDone:
+  			if *debugOutput {describe(timerDone)}
+  			fmt.Println("\n *** Timmer has elapsed! Pencils DOWN!! ***")
+  			codeQuizDone = 1
+  			isRunning = false
+  		}
+  	}
+	if *debugOutput {describe(quizAnswers)}
+  	gradeQuiz(quizProblems, quizAnswers)
+ 	exit("Quiz is done!", codeQuizDone)
+}
+
+var csvFilename = strings.NewReader(`5+5,10
+1+1,2
+8+3,11
+1+2,3
+8+6,14
+3+1,4
+1+4,5
+5+1,6
+2+3,5
+3+3,6
+2+4,6
+5+2,7
+`)
+
